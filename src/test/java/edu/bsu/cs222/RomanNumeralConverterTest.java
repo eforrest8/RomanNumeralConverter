87b5375d44ef2ae4f3d2a452a5d9b54976551321
Ethan Forrest <ethan.forrest@bsu.edu>
2022-01-19 22:22:18 -0500

    Added test for "III" -> 3

--- src/test/java/edu/bsu/cs222/RomanNumeralConverterTest.java
+++ src/test/java/edu/bsu/cs222/RomanNumeralConverterTest.java
@@ -11,8 +11,8 @@ public class RomanNumeralConverterTest {
     @ParameterizedTest
     @CsvSource({"I,1","i,1","V,5","v,5","X,10","x,10","L,50","l,50",
             "C,100","c,100","D,500","d,500","M,1000","m,1000",
-            "II,2","VV,10","XX,20","IV,4","VI,6","IX,9","VX,5","XI,11",
-            "XV,15","IVX,6","VIX,4"})
+            "II,2","VV,10","XX,20","III,3",
+            "IV,4","VI,6","IX,9","VX,5","XI,11","XV,15","IVX,6","VIX,4"})
     public void testConverter(String numeral, int expected) throws Exception {
         int actual = converter.convert(numeral);
         Assertions.assertEquals(expected, actual);
